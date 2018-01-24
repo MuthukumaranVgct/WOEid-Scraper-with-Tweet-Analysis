8dc2628bf5416df4a02285dd437ea080de17654a
srmanoj <35737274+srmanoj@users.noreply.github.com>
2018-01-24 08:37:29 +0530

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -1,1 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib.parse as u
+s = requests.Session()
+def fetch(url, data=None):
+	if data is None:
+		return s.get(url).content
+	else:
+		return s.post(url, data=data).content
+def Scraping():
+	city=input("Enter a City Name or Country Or a State Name")
+	URL = 'http://woeid.rosselliot.co.nz/'
+	
 #put your code here
